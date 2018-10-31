f57ba3e6c3cf75c5ecfd204ad6d5133de58a5565
William Lu <rlu1@stuy.edu>
2018-10-30 22:19:41 -0400

    ~

--- stat_driver.c
+++ stat_driver.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <time.h>
-#include <stdlib.h>
+#include <string.h>
+
+char* get_permission(mode_t m){
+  char* permission = malloc(11);
+  strcpy(permission, "-");
+  m %= 01000;
+  char str[] = "r\0w\0x";
+  char l[9];
+  for (int i = 0; i < 9; i ++){
+    l[8 - i] = m % 2;
+    m /= 2;
+  }
+
+  int i;
+  for (i = 0; i < 9; i ++)
+    if (l[i])
+      strcat(permission, &str[2 * (i % 3)]);
+    else
+      strcat(permission, "-");
+
+  return permission;
+}
 
 int main(){
   struct stat *a = malloc(sizeof(struct stat));
@@ -9,10 +31,19 @@ int main(){
   printf("\nfile size: %ld\n", a->st_size);
   printf("mode in octal: %o\n", a->st_mode);
   printf("time of last access: %s\n", ctime(&(a->st_atime)));
+
   printf("readable file size:\n");
-  printf("\t%ldB\n", a->st_size);
-  printf("\t%ldKB\n", (a->st_size / 1024.0));
-  printf("\t%ldMB\n", (a->st_size / 1024.0 / 1024.0);
-  printf("\t%ldGB\n", (a->st_size / 1024.0 / 1024.0 / 1024.0);
-  
+  double size = a->st_size;
+  printf("\t%ldB\n", (long)size);
+  size /= 1024.0;
+  printf("\t%lfKB\n", size);
+  size /= 1024.0;
+  printf("\t%lfMB\n", size);
+  size /= 1024.0;
+  printf("\t%lfGB\n", size);
+
+  printf("\n");
+
+  printf("readable permission:\n");
+  printf("\t%s\n", get_permission(a->st_mode));
 }
